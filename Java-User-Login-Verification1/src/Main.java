5404a6318142ee43c6c958e2e8a3d82bd628be41
Joshua Miller <jamiller38@gmail.com>
2022-12-15 20:46:20 -0500

    adding scanner class and attempts, also added a boolean value of 0 for count "++"

--- Java-User-Login-Verification1/src/Main.java
+++ Java-User-Login-Verification1/src/Main.java
@@ -1,6 +1,14 @@
 class validatorTest{
     public static void main(String[] args) {
 
-        System.out.println("test");
+        //introducing scanner to pick up login info
+        Scanner login = new Scanner(System.in);
+
+        // setting boolean to true, while this is true the user will be prompted to log in with correct credentials
+        boolean isUserVerified = true;
+
+        //Number of tries/guesses it took for the user to log in to the system.
+        int loginAttempts = 0;
+
     }
 }
