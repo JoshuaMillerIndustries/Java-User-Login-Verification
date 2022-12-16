304bc80cab8ca29cba973be5490e3374d67566ae
Joshua Miller <jamiller38@gmail.com>
2022-12-15 20:47:43 -0500

    added remaining program and features. refer to code comments for further code details

--- Java-User-Login-Verification1/src/Main.java
+++ Java-User-Login-Verification1/src/Main.java
@@ -10,5 +10,31 @@ class validatorTest{
         //Number of tries/guesses it took for the user to log in to the system.
         int loginAttempts = 0;
 
+
+        //this while loop will loop until " DarkKnight is entered into the terminal ending the program
+        while (isUserVerified){
+            System.out.println("Enter Recognized Verification Key: ");
+
+            // Declaring the user input as a string for java to compare to match the correct input to complete the program
+            String loginAttempt = login.next();
+            loginAttempts++;
+
+
+            // an if statement to state that if "DarkKnight" is passed the terminal will alert the user of the amount of attempts.
+            if (loginAttempt.equals("DarkKnight")){
+
+                isUserVerified = false;
+
+                System.out.println("This Attempt Was Clocked at " + loginAttempts + " Attempts");
+
+            }else{
+                // while "DarkKnight is not passed the user will continuously be prompted to enter verification key
+                System.out.println("Failed");
+
+            }
+
+        }
+
+
     }
 }
